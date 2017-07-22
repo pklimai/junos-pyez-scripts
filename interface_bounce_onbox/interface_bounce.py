46b4a63afa95274366c57dc6df71935b7fd326a8
pklimai <pklimai@users.noreply.github.com>
2017-07-22 20:23:18 +0300

    Update interface_bounce.py

--- interface_bounce_onbox/interface_bounce.py
+++ interface_bounce_onbox/interface_bounce.py
@@ -1,15 +1,15 @@
-from jnpr.junos import Device
+from jnpr.junos import Device                    # (1)
 from jnpr.junos.utils.config import Config
 from jnpr.junos.exception import *
 import argparse
 from time import sleep
 
-arguments = {
+arguments = {                                    # (2)
     "interface": "Name of the interface to disable/enable",
     "delay": "Time to wait before enabling the interface (seconds)",
 }
 
-def config_xml(interface_name, disable_attributes): 
+def config_xml(interface_name, disable_attributes):      # (3)
     return """
         <configuration>
             <interfaces>
@@ -21,7 +21,7 @@ def config_xml(interface_name, disable_attributes):
         </configuration>
     """.format(interface_name, disable_attributes)
 
-def change_config(dev, delta_config, log_message):
+def change_config(dev, delta_config, log_message):       # (4)
     print "%s: Locking the configuration" % log_message
     try:
         dev.cu.lock()
@@ -63,22 +63,23 @@ def change_config(dev, delta_config, log_message):
     return True
 
 
-def main():
-    parser = argparse.ArgumentParser()
+def main():                                      # (5)
+    parser = argparse.ArgumentParser()           # (6) 
     for key in arguments:
         parser.add_argument(('-' + key), required=True, help=arguments[key])
-
     args = parser.parse_args()
 
-    with Device() as dev:
-        dev.bind( cu=Config )
-        if change_config(dev, config_xml(args.interface, ""), "Disabling interface"):
+    with Device() as dev:                        # (7)
+        dev.bind( cu=Config )                    # (8) 
+        if change_config(dev, config_xml(args.interface, ""),     # (9)
+                "Disabling interface"):
             print "Waiting %s seconds..." % args.delay
-            sleep(float(args.delay))
-            if change_config(dev, config_xml(args.interface, "delete='delete'"), "Enabling interface"):
+            sleep(float(args.delay))                              # (10)
+            if change_config(dev, config_xml(args.interface, "delete='delete'"), 
+                    "Enabling interface"):                        # (11)
                 print "Interface bounce script finished successfully."
             else:
                 print "Error enabling the interface, it will remain disabled."
     
-if __name__ == "__main__":
+if __name__ == "__main__":                                        # (12) 
     main()
