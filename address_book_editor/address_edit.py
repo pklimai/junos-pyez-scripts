6a56f27ee588ed49f3448a2cc208781779d9f1fa
pklimai <pklimai@users.noreply.github.com>
2017-08-03 19:52:30 +0300

    Create address_edit.py

---
+++ address_book_editor/address_edit.py
@@ -0,0 +1,165 @@
+from jnpr.junos import Device
+from jnpr.junos.utils.config import Config
+from lxml import etree
+import ipaddress
+
+USER = "lab"
+PASSWD = "lab123"
+DEVICE_IP = "10.254.0.35"
+
+ADDR_BOOK_NAME = "global"
+ADDR_SET_NAME = "ALLOWED-IN"
+ADDR_NAME_PREFIX = "CIDR-"
+
+STR_INVITE = """
+Address-set editor script for Juniper SRX. Device: {0}
+Address book to edit: {1}   Address-set to edit: {2}
+  r - READ and show addresses
+  a - ADD address (IP or subnet)
+  d - DELETE particular address
+  q - QUIT script
+Choice >>> """
+
+STR_QUITING = "Goodbye!"
+STR_READING = "\nReading and displaying address book entries:"
+STR_UNKNOWN_INPUT = "Unknown input, please repeat."
+STR_ENTER_IP_ADD = "Enter IP or IP/mask to add >>> "
+STR_ADDRESS_ADDED = "Address added successfully."
+STR_INVALID_IP = "Invalid entry, please repeat."
+STR_ENTER_IP_DEL = "Enter IP/mask to delete >>> "
+STR_ADDRESS_DELETED = "Address deleted successfully."
+STR_PDIFF_BANNER = "Config diff on the device:"
+
+STR_GET_CONFIG = """<configuration>
+                        <security>
+                            <address-book>
+                                <name>{0}</name>
+                            </address-book>
+                        </security>
+                    </configuration>"""
+
+
+class InconsistentConfigException(Exception):
+    pass
+
+
+# All addresses in address set that start with prefix ADDR_NAME_PREFIX, must
+# be of 'standard' form (prefix + cidr = cidr)
+def read_addresses():
+    addr_book_prefixes = set()       # empty sets so far
+    address_set_prefixes = set()
+    with Device(host=DEVICE_IP, user=USER, password=PASSWD) as dev:
+        resp = dev.rpc.get_config(
+                filter_xml=etree.XML(STR_GET_CONFIG.format(ADDR_BOOK_NAME)),
+                options={'inherit': 'inherit', 'database': 'committed', 'format': 'XML'})
+        if resp is not None:
+            for address_element in resp.findall("security/address-book/address"):
+                name = address_element.findtext("name")
+                ip_prefix = address_element.findtext("ip-prefix")
+                if name is not None and ip_prefix is not None:
+                    if name == ADDR_NAME_PREFIX + ip_prefix:
+                        addr_book_prefixes.add(ip_prefix)
+                    else:         # this entry was not added by script - ignore
+                        pass
+
+            for address_set_element in resp.findall("security/address-book/address-set[name='{0}']/address"
+                                                            .format(ADDR_SET_NAME)):
+                ab_name = address_set_element.findtext("name")
+                if ab_name.startswith(ADDR_NAME_PREFIX):
+                    test_ip = ab_name[len(ADDR_NAME_PREFIX):]
+                    if test_ip in addr_book_prefixes:
+                        address_set_prefixes.add(test_ip)
+                    else:
+                        raise InconsistentConfigException("Inconsistent entry in address book")
+
+    return address_set_prefixes
+
+
+def display_addresses(addrs):
+    for addr in sorted(addrs):
+        print(addr)
+
+
+def sanitize_ip(address_entered):
+    result = address_entered
+    if "/" not in result: result += "/32"
+    try:
+        ip = ipaddress.IPv4Network(result)
+    except:
+        return None
+    return result
+
+
+STR_SET_CONFIG = """set security address-book {0} address {1}{2} {2}
+set security address-book global address-set ALLOWED-IN address {1}{2}"""
+
+
+def change_config_with_set_commands(conf, set_commands):
+    conf.lock()
+    conf.load(set_commands, format="set")
+    print(STR_PDIFF_BANNER)
+    conf.pdiff()
+    conf.commit()
+    conf.unlock()
+
+
+def add_address(address_sanitized):
+    with Device(host=DEVICE_IP, user=USER, password=PASSWD) as dev:
+        with Config(dev) as conf:
+            change_config_with_set_commands(conf, STR_SET_CONFIG.format(
+                ADDR_BOOK_NAME, ADDR_NAME_PREFIX, address_sanitized))
+
+
+STR_DELETE_CONFIG = """delete security address-book {0} address {1}{2} {2}
+delete security address-book global address-set ALLOWED-IN address {1}{2}"""
+
+
+def del_address(address_sanitized):
+    with Device(host=DEVICE_IP, user=USER, password=PASSWD) as dev:
+        with Config(dev) as conf:
+            change_config_with_set_commands(conf, STR_DELETE_CONFIG.format(
+                ADDR_BOOK_NAME, ADDR_NAME_PREFIX, address_sanitized))
+
+
+def main():
+    #addrs = []
+    while True:
+        print(STR_INVITE.format(DEVICE_IP, ADDR_BOOK_NAME, ADDR_SET_NAME), end="")
+        choice = input().lower()
+        if choice == "q":
+            print(STR_QUITING)
+            break
+        elif choice == "r":
+            print(STR_READING)
+            addrs = read_addresses()
+            display_addresses(addrs)
+        elif choice == "a":
+            print(STR_ENTER_IP_ADD, end="")
+            address_entered = input()
+            address_sanitized = sanitize_ip(address_entered)
+            if address_sanitized is None:
+                print(STR_INVALID_IP)
+            else:
+                add_address(address_sanitized)
+                print(STR_ADDRESS_ADDED)
+        elif choice == "d":
+            print(STR_ENTER_IP_DEL, end="")
+            address_entered = input()
+            address_sanitized = sanitize_ip(address_entered)
+            if address_sanitized is None:
+                print(STR_INVALID_IP)
+            else:
+                del_address(address_sanitized)
+                print(STR_ADDRESS_DELETED)
+        else:
+            print(STR_UNKNOWN_INPUT)
+            address_entered = input()
+            address_sanitized = sanitize_ip(address_entered)
+            if address_sanitized is None:
+                print(STR_INVALID_IP)
+            else:
+                del_address(address_sanitized)
+                print(STR_ADDRESS_DELETED)
+
+if __name__ == "__main__":
+    main()
