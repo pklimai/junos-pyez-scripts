b3b3fc52fae72af8432696019d4f7c70731957bb
pklimai <pklimai@users.noreply.github.com>
2017-08-16 22:19:42 +0300

    Update provision_l3vpn.py

--- provision_l3vpn/provision_l3vpn.py
+++ provision_l3vpn/provision_l3vpn.py
@@ -1,50 +1,58 @@
-import jinja2
-import os
-import yaml
-from pprint import pprint
+#!/usr/bin/python3
+
 from jnpr.junos import Device
 from jnpr.junos.utils.config import Config
 from jnpr.junos.exception import *
+import jinja2
+import os
+import yaml
 
 USER = "lab"
 PASSWD = "lab123"
 
-def render(tpl_path, context):
-    path, filename = os.path.split(tpl_path)
-    return jinja2.Environment(
-        loader=jinja2.FileSystemLoader(path or './')
-    ).get_template(filename).render(context)
+def render(full_filename, context):
+    path, filename = os.path.split(full_filename)
+    template = jinja2.Environment(
+        loader=jinja2.FileSystemLoader(path or './')).get_template(filename)
+    return template.render(context)
 
-with open("l3vpn-data.yaml") as var_file:
-    l3vpn_data = yaml.load(var_file)
+def main():
+    with open("l3vpn-data.yaml") as var_file:
+        l3vpn_data = yaml.load(var_file)
 
-for PE in l3vpn_data["PEs"]:
-    print("\n\nWorking on PE %s" % PE)
-    z = {}
-    z.update(l3vpn_data["PEs"][PE])
-    z.update({"customers": l3vpn_data["customers"]})
-    pprint(z)
-    result_conf = render("l3vpn_config.jinja2", z)
-    print(result_conf)
+    for PE in l3vpn_data["PEs"]:
+        print("Working on device %s" % PE)
+        vars = l3vpn_data["PEs"][PE].copy()
+        vars.update({"customers": l3vpn_data["customers"]})
+        result_conf = render("l3vpn_config.jinja2", vars)
 
-    try:
-        with Device(host=l3vpn_data["PEs"][PE]["management_ip"], user=USER, password=PASSWD) as dev:
-            # open and close is done automatically by context manager
-            with Config(dev, mode="exclusive") as conf:
-                # exclusive locks are treated automatically by context manager
-                conf.load(result_conf, format="text")
-                #print(STR_PDIFF_BANNER)
-                conf.pdiff()
-                conf.commit()
-    except LockError:
-        print("\n\nError applying config: configuration was locked!")
-    except ConnectRefusedError:
-        print("\n\nError: Device connection refused!")
-    except ConnectTimeoutError:
-        print("\n\nError: Device connection timed out!")
-    except ConnectAuthError:
-        print("\n\nError: Authentication failure!")
-    except ConfigLoadError as ex:
-        print("\n\nError: " + str(ex))
-    else:
-        print("Config applied successfully!")
+        try:
+            with Device(host=l3vpn_data["PEs"][PE]["management_ip"],
+                        user=USER, password=PASSWD) as dev:
+                # open and close is done automatically by context manager
+                with Config(dev, mode="exclusive") as conf:
+                    # exclusive locks are treated automatically by context manager
+                    conf.load(result_conf, format="text")
+                    diff = conf.diff()
+                    if diff is None:
+                        print("Configuration is up to date.")
+                    else:
+                        print("Config diff to be committed on device:")
+                        print(diff)
+                        conf.commit()
+        except LockError:
+            print("\nError applying config: configuration was locked!")
+        except ConnectRefusedError:
+            print("\nError: Device connection refused!")
+        except ConnectTimeoutError:
+            print("\nError: Device connection timed out!")
+        except ConnectAuthError:
+            print("\nError: Authentication failure!")
+        except ConfigLoadError as ex:
+            print("\nError: " + str(ex))
+        else:
+            if diff is not None:
+                print("Config committed successfully!")
+
+if __name__ == "__main__":
+    main()
