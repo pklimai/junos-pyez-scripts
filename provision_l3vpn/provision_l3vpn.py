0782f22e89f26e56a053f82fc94068b7e8d98969
pklimai <pklimai@users.noreply.github.com>
2017-08-17 14:53:39 +0300

    Update provision_l3vpn.py

--- provision_l3vpn/provision_l3vpn.py
+++ provision_l3vpn/provision_l3vpn.py
@@ -1,45 +1,45 @@
 #!/usr/bin/python3
 
-from jnpr.junos import Device
+from jnpr.junos import Device                              # (1)
 from jnpr.junos.utils.config import Config
 from jnpr.junos.exception import *
 import jinja2
 import os
 import yaml
 
-USER = "lab"
+USER = "lab"                                               # (2)
 PASSWD = "lab123"
 
-def render(full_filename, context):
+def render(full_filename, context):                        # (3)
     path, filename = os.path.split(full_filename)
     template = jinja2.Environment(
         loader=jinja2.FileSystemLoader(path or './')).get_template(filename)
     return template.render(context)
 
-def main():
+def main():                                                # (4)
     with open("l3vpn-data.yaml") as var_file:
         l3vpn_data = yaml.load(var_file)
 
-    for PE in l3vpn_data["PEs"]:
+    for PE in l3vpn_data["PEs"]:                                # (5)
         print("Working on device %s" % PE)
-        vars = l3vpn_data["PEs"][PE].copy()
+        vars = l3vpn_data["PEs"][PE].copy()                     # (6)
         vars.update({"customers": l3vpn_data["customers"]})
-        result_conf = render("l3vpn_config.jinja2", vars)
+        result_conf = render("l3vpn_config.jinja2", vars)       # (7)
 
-        try:
+        try:                                                    # (8)
             with Device(host=l3vpn_data["PEs"][PE]["management_ip"],
                         user=USER, password=PASSWD) as dev:
                 # open and close is done automatically by context manager
                 with Config(dev, mode="exclusive") as conf:
                     # exclusive locks are treated automatically by context manager
                     conf.load(result_conf, format="text")
-                    diff = conf.diff()
+                    diff = conf.diff()                          # (9)
                     if diff is None:
                         print("Configuration is up to date.")
                     else:
                         print("Config diff to be committed on device:")
                         print(diff)
-                        conf.commit()
+                        conf.commit()                           # (10)
         except LockError:
             print("\nError applying config: configuration was locked!")
         except ConnectRefusedError:
@@ -50,9 +50,9 @@ def main():
             print("\nError: Authentication failure!")
         except ConfigLoadError as ex:
             print("\nError: " + str(ex))
-        else:
+        else:                                                   # (11)
             if diff is not None:
                 print("Config committed successfully!")
 
-if __name__ == "__main__":
+if __name__ == "__main__":                                      # (12)
     main()
